6e9e0803b9b493bc454d632908325853afff76d8
devgis <devgis@163.com>
2022-05-17 10:00:51 +0800

    Backuped
    Backuped

---
+++ Source/XMLImport/Main.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace XMLImport
+{
+    partial class Main
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btImport = new System.Windows.Forms.Button();
+            this.tbPath = new System.Windows.Forms.TextBox();
+            this.btSelectPath = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btImport
+            // 
+            this.btImport.Location = new System.Drawing.Point(550, 82);
+            this.btImport.Name = "btImport";
+            this.btImport.Size = new System.Drawing.Size(75, 23);
+            this.btImport.TabIndex = 0;
+            this.btImport.Text = "导入";
+            this.btImport.UseVisualStyleBackColor = true;
+            this.btImport.Click += new System.EventHandler(this.btImport_Click);
+            // 
+            // tbPath
+            // 
+            this.tbPath.Location = new System.Drawing.Point(54, 45);
+            this.tbPath.Name = "tbPath";
+            this.tbPath.Size = new System.Drawing.Size(490, 21);
+            this.tbPath.TabIndex = 1;
+            // 
+            // btSelectPath
+            // 
+            this.btSelectPath.Location = new System.Drawing.Point(550, 43);
+            this.btSelectPath.Name = "btSelectPath";
+            this.btSelectPath.Size = new System.Drawing.Size(75, 23);
+            this.btSelectPath.TabIndex = 2;
+            this.btSelectPath.Text = "选择";
+            this.btSelectPath.UseVisualStyleBackColor = true;
+            this.btSelectPath.Click += new System.EventHandler(this.btSelectPath_Click);
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(683, 142);
+            this.Controls.Add(this.btSelectPath);
+            this.Controls.Add(this.tbPath);
+            this.Controls.Add(this.btImport);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Main";
+            this.Text = "导入";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btImport;
+        private System.Windows.Forms.TextBox tbPath;
+        private System.Windows.Forms.Button btSelectPath;
+    }
+}
+
---
+++ Source/XMLImport/Main.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace XMLImport
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void btImport_Click(object sender, EventArgs e)
+        {
+            if (System.IO.Directory.Exists(tbPath.Text))
+            {
+                string[] xmls = Directory.GetFiles(tbPath.Text, "*.xml");
+                foreach (string sxml in xmls)
+                {
+                    ImportFile(sxml);
+                }
+            }
+            else
+            {
+                MessageBox.Show("目录不存在！");
+            }
+        }
+
+        private bool ImportFile(string FileName)
+        {
+            if (File.Exists(FileName))
+            {
+                XmlDocument xmldoc = new XmlDocument();
+                xmldoc.Load(FileName);
+                XmlNode xn = xmldoc.SelectSingleNode("PEOPLE");
+                foreach (XmlNode xn1 in xn.ChildNodes)
+                {
+                    // 将节点转换为元素，便于得到节点的属性值
+                    XmlElement xe = (XmlElement)xn1;
+                    string PERSONID = xe.GetAttribute("PERSONID");
+                    string NAME = xe.GetElementsByTagName("NAME").Item(0).InnerText;
+                    string ADDRESS = xe.GetElementsByTagName("ADDRESS").Item(0).InnerText;
+                    string TEL = xe.GetElementsByTagName("TEL").Item(0).InnerText;
+                    string EMAIL = xe.GetElementsByTagName("EMAIL").Item(0).InnerText;
+                    MessageBox.Show(string.Format("PERSONID:{0} NAME:{1} ADDRESS:{2} TEL:{3} EMAIL:{4}", PERSONID, NAME, ADDRESS, TEL, EMAIL));
+                }
+
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        private void btSelectPath_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+            if (fbd.ShowDialog() == DialogResult.OK)
+            {
+                tbPath.Text = fbd.SelectedPath;
+            }
+        }
+    }
+}
